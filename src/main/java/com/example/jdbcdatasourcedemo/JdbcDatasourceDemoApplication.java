63002991328260f42d6709f09f6ed057637d3a33
Maureen Josephine <Maureen Josephine>
2020-03-16 23:38:09 +0300

    initial commit

---
+++ src/main/java/com/example/jdbcdatasourcedemo/JdbcDatasourceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdbcdatasourcedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcDatasourceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcDatasourceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jdbcdatasourcedemo/JdbcDatasourceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbcdatasourcedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcDatasourceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
